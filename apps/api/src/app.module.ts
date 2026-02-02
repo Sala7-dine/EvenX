8d210f47a95f8f7872c4d41374402523be9aa3eb
salahdine <salahdaha7@gmail.com>
2026-02-02 15:31:16 +0100

    EV-9 feat: Implement user authentication and registration with JWT, Mongoose.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://salahdaha7_db_user:Fr0Aw7KHAPDnsu5t@cluster0.ixl0arc.mongodb.net/EvenX?retryWrites=true&w=majority'),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

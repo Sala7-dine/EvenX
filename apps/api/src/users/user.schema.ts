3e2627ca3203aac99f091326619325d43739e2f1
salahdine <salahdaha7@gmail.com>
2026-02-03 15:38:22 +0100

    feat: Add unit tests for authentication, reservation, and event services, and explicitly define the user role schema as a string type.

--- apps/api/src/users/user.schema.ts
+++ apps/api/src/users/user.schema.ts
@@ -16,7 +16,7 @@ export class User {
   @Prop({ required: true })
   password?: string;
 
-  @Prop({ required: true, enum: Role, default: Role.PARTICIPANT })
+  @Prop({ required: true, enum: Role, default: Role.PARTICIPANT, type: String })
   role: Role;
 
 }
